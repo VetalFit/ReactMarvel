1e646fd134be26be6508587d24a2405a042bc8d7
Vitalii Malyshev <v95664191@gmail.com>
2023-12-28 20:46:56 +0200

    second commit

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,81 @@
+import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
+import MarvelService from '../../services/MarvelService';
+
+import './charList.scss';
+
+class CharList extends Component {
+	state = {
+		charList: [],
+		loading: true,
+		error: false
+	}
+
+	marvelService = new MarvelService()
+
+	componentDidMount() {
+		this.marvelService.getAllCharacters()
+			.then(this.onCharListLoaded)
+			.catch(this.onError)
+	}
+
+	onCharListLoaded = (charList) => {
+		this.setState({
+			charList,
+			loading: false
+		})
+	}
+
+	onError = () => {
+		this.setState({
+			loading: false,
+			error: true
+		})
+	}
+
+	renderChar = (arr) => {
+		const items = arr.map(item => {
+			let imgStyle = { 'objectFit': 'cover' }
+			if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+				imgStyle = { 'objectFit': 'unset' }
+			}
+			return (
+				<li className="char__item"
+					key={item.id} >
+					<img src={item.thumbnail} alt="abyss" style={imgStyle} />
+					<div className="char__name">{item.name}</div>
+				</li >
+			)
+		});
+
+		return (
+			<ul className="char__grid" >
+				{items}
+			</ul>
+		)
+	}
+
+	render() {
+		const { charList, error, loading } = this.state;
+		const errorMessage = error ? <ErrorMessage /> : null;
+		const spinner = loading ? <Spinner /> : null;
+		const items = this.renderChar(charList)
+		const content = !(loading || error) ? items : null
+
+
+		return (
+			<div className="char__list">
+				{errorMessage}
+				{spinner}
+				{content}
+				<button className="button button__main button__long">
+					<div className="inner">load more</div>
+				</button>
+			</div>
+		)
+	}
+}
+
+export default CharList;
